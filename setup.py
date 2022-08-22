ae052044214dad3b46ca1b845ebc3096e7d05967
jagg3127 <jhaught2007@gmail.com>
2022-08-22 13:07:57 -0400

    largest update

--- setup.py
+++ setup.py
@@ -1,21 +1,16 @@
-import setuptools
+from setuptools import setup, find_packages
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setuptools.setup(
-    name='keys',
-    version='0.1.32',
+setup(
+    name='keys_school',
+    version='0.0.1',
     author='N/A',
     author_email='joshua.haught@rollinghills.k12.oh.us',
-    description='School package for keypress recognition',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url='https://github.com/jagg3127/lib',
-    project_urls = {
-        "Bug Tracker": "https://github.com/jagg3127/lib/issues"
-    },
-    license='MIT',
-    packages=['keys'],
-    install_requires=['pynput'],
+    project_urls = {"Bug Tracker": "https://github.com/jagg3127/lib/issues"},
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    install_requires=['getkey']
 )
